cefc3ad7ed3db98532aa38f0f271c24551746ae7
NpbTruong <truong.nguyen@whl.travel>
2026-01-14 16:28:12 +0700

    Update app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hi!'),
+          child: Text('Hi ho!'),
         ),
       ),
     );
