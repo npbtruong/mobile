1e30136733ac03a574c2e78fdc53a53dfac7297e
npbtruong <npbtruong@gmail.com>
2026-01-31 17:38:42 +0700

    #

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,7 @@
 class ApiConstants {
   // For Android emulator use 10.0.2.2 to reach the host machine.
-  static const String host = 'http://10.0.2.2:8000';
+  // For Real Android device use 192.168.1.3 to reach the host machine.
+  static const String host = 'http://192.168.1.3:8000';
 
   // Base URL for API endpoints (used by Dio baseUrl).
   static const String baseUrl = '$host/api/';
